cc8c7aeeaa2d276cecc320a44b6eeb84934b31db
nikhila-naidu <nikhilanaidu.kaparapu@gmail.com>
2023-02-23 19:18:39 +0530

    chore: quick update on about page

--- components/about/about1/PersonalInfo.jsx
+++ components/about/about1/PersonalInfo.jsx
@@ -3,22 +3,22 @@ import {
   FaMapMarkerAlt,
   FaMobileAlt,
   FaRegCalendarAlt,
-} from "react-icons/fa";
+} from 'react-icons/fa';
 
 const PersonalInfo = () => {
   const personalContent = [
     {
       id: 1,
       icon: <FaMobileAlt />,
-      iconColor: "text-oriange",
-      name: "Phone",
+      iconColor: 'text-oriange',
+      name: 'Phone',
       meta: (
         <>
           <a
-            className="hover:text-[#FA5252] duration-300 transition"
-            href="tel:+1234567890"
+            className='hover:text-[#FA5252] duration-300 transition'
+            href='tel:+1234567890'
           >
-            +123 456 7890
+            9398614418
           </a>
         </>
       ),
@@ -26,23 +26,23 @@ const PersonalInfo = () => {
     {
       id: 2,
       icon: <FaMapMarkerAlt />,
-      iconColor: "text-oriange-lite",
-      name: "Location",
-      meta: <>Hong kong china</>,
+      iconColor: 'text-oriange-lite',
+      name: 'Location',
+      meta: <>visakhapatnam</>,
     },
     {
       id: 3,
       icon: <FaEnvelopeOpenText />,
-      iconColor: "text-green",
-      name: "Email",
+      iconColor: 'text-green',
+      name: 'Email',
       meta: (
         <>
-          {" "}
+          {' '}
           <a
-            className="hover:text-[#FA5252] duration-300 transition"
-            href="mailto:ibthemes21@gmail.com"
+            className='hover:text-[#FA5252] duration-300 transition'
+            href='mailto:ibthemes21@gmail.com'
           >
-            example@mail.com
+            nikhilanaidu.kaparapu@gmail.com
           </a>
         </>
       ),
@@ -50,26 +50,26 @@ const PersonalInfo = () => {
     {
       id: 4,
       icon: <FaRegCalendarAlt />,
-      iconColor: "text-color-50",
-      name: "Birthday",
-      meta: <>May 27, 1990</>,
+      iconColor: 'text-color-50',
+      name: 'Birthday',
+      meta: <>Dec 17, 1998</>,
     },
   ];
 
   return (
     <>
       {personalContent.map((item) => (
-        <div className="flex" key={item.id}>
+        <div className='flex' key={item.id}>
           <span
             className={`${item.iconColor} dark:bg-color-990 shadow-icon mr-2.5 flex items-center justify-center rounded-md text-2xl w-12 text-`}
           >
             {item.icon}
           </span>
-          <div className="space-y-1">
-            <p className="text-xs text-gray-lite dark:text-color-910">
+          <div className='space-y-1'>
+            <p className='text-xs text-gray-lite dark:text-color-910'>
               {item.name}
             </p>
-            <h6 className="font-medium dark:text-white">{item.meta}</h6>
+            <h6 className='font-medium dark:text-white'>{item.meta}</h6>
           </div>
         </div>
       ))}
