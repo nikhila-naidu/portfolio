abcce48468f0e33cf6070150e0de81feeeecbee1
nikhila-naidu <nikhilanaidu.kaparapu@gmail.com>
2023-02-24 12:07:15 +0530

    chore: refactor

--- data/resumeData.js
+++ data/resumeData.js
@@ -23,13 +23,13 @@ module.exports = [
         bg: '#FFF1FB',
       },
 
-      // {
-      //   id: 3,
-      //   date: '2021',
-      //   title: 'Best Developer ',
-      //   place: 'University Of Melbourne, NA',
-      //   bg: '#FFF4F4',
-      // },
+      {
+        id: 3,
+        date: '2021',
+        title: 'Best Developer ',
+        place: 'University Of Melbourne, NA',
+        bg: '#FFF4F4',
+      },
     ],
   },
   {
@@ -62,34 +62,52 @@ module.exports = [
       },
     ],
   },
-  // {
-  //   id: 3,
-  //   title: 'Awards',
-  //   icon: <FaAward />,
-  //   items: [
-  //     {
-  //       id: 1,
-  //       date: '2015-2017',
-  //       title: '  Graphic Designer',
-  //       place: 'Web Graphy, Los Angeles, CA',
-  //       bg: '#FCF4FF',
-  //     },
+  {
+    id: 3,
+    title: 'Projects',
+    icon: <FaAward />,
+    items: [
+      {
+        id: 1,
+        date: '2015-2017',
+        title: ' Todo App ',
+        place: (
+          <>
+            <p>
+              <a href='/todolist'>Todolist</a>
+            </p>
+          </>
+        ),
+        bg: '#FCF4FF',
+      },
 
-  //     {
-  //       id: 2,
-  //       date: '2014 - 2015',
-  //       title: 'Jr. Web Developer',
-  //       place: 'Creative Gigs.',
-  //       bg: '#FCF9F2',
-  //     },
+      {
+        id: 2,
+        date: '2014 - 2015',
+        title: 'Mern',
+        place: (
+          <>
+            <p>
+              <a href='/mern'>Mern</a>
+            </p>
+          </>
+        ),
+        bg: '#FCF9F2',
+      },
 
-  //     {
-  //       id: 3,
-  //       date: '2015-2017',
-  //       title: 'Best Freelancer',
-  //       place: 'Fiver & Upwork Level 2 & Top Rated',
-  //       bg: '#FCF4FF',
-  //     },
-  //   ],
-  // },
+      {
+        id: 3,
+        date: '2015-2017',
+        title: 'Coffee Connoisseur',
+        place: (
+          <>
+            <p>
+              <a href='/coffee connoisseur'>coffee</a>
+            </p>
+          </>
+        ),
+        bg: '#FCF4FF',
+      },
+    ],
+  },
 ];
